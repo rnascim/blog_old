f67461ed368e4a8373a06db4e5ab7fe8c58b4263
Rogerio Nascimento <rnascim@gmail.com>
2010-01-24 12:27:51 -0200

    Criar campo 'commenter' nos comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,27 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comment])
+    # @comment = @post.comments.create!(params[:comment])
+    comment = @post.comments.new(params[:comment])
     # redirect_to @post
     respond_to do |format|
-      format.html { redirect_to @post }
+      if comment.save
+        flash[:notice] = 'Post was successfully created.'
+        format.html { redirect_to @post }        
+      else
+        flash[:notice] = 'Campos do comentário não foram corretamente preenchidos'
+        format.html { redirect_to @post }   
+      end
     end
   end
+  
+  # if @post.save
+  #   flash[:notice] = 'Post was successfully created.'
+  #   format.html { redirect_to(@post) }
+  #   format.xml  { render :xml => @post, :status => :created, :location => @post }
+  # else
+  #   format.html { render :action => "new" }
+  #   format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+  # end
+  
 end
